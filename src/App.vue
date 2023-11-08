5d835ae6e3919a12dc32d7c3a680967f6a6ad3a2
Henrique Kalife <henriquekalife@gmail.com>
2023-11-07 21:08:41 -0300

    chore: add full base project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import PeopleList from './components/PeopleList.vue';
+import ProductList from './components/ProductList.vue';
+
+</script>
+
+<template>
+  <div>
+    <h1>FRONTEND PERFORMANCE (VUE)</h1>
+
+    <PeopleList />
+    <ProductList />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
---
+++ src/components/PeopleList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h3>People</h3>
+    <table>
+      <tr>
+        <th>Head 1</th>
+        <th>Head 2</th>
+        <th>Head 3</th>
+      </tr>
+      <tr>
+        <td>Body 1</td>
+        <td>Body 2</td>
+        <td>Body 3</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+</style>
